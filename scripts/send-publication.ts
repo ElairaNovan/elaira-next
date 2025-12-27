0b6f7d95f15b5956565fb4116ac009f83c7efaeb
Elaira Novan <zluwkausa@gmail.com>
2025-12-26 18:29:04 -0800

    update

--- scripts/send-publication.ts
+++ scripts/send-publication.ts
@@ -1,4 +1,6 @@
 // scripts/send-publication.ts
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 
 import { Resend } from "resend";
 import { publications } from "../src/content/publications";
